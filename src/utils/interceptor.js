33d5d62fd4ad2ae6cee552ffd92cb00809768893
question7892 <1499728683@qq.com>
2026-06-14 15:28:18 +0800

    fix: 更新后端API地址为直连IP & 优化devServer配置

--- src/utils/interceptor.js
+++ src/utils/interceptor.js
@@ -1,11 +1,11 @@
 // 根路由
-const BASE_URL = "https://czqtolearn.mynatapp.cc"
+const BASE_URL = "http://59.110.163.182"
 
 // add token
 function addToken(args) {
   if (uni.getStorageSync("token")) {
-    if (args.header) return (args.header.authorization = uni.getStorageSync("token"))
-    args.header = { authorization: uni.getStorageSync("token") }
+    if (args.header) return (args.header.token = uni.getStorageSync("token"))
+    args.header = { token: uni.getStorageSync("token") }
   }
 }
 
